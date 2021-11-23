b6cd033e492f0e119c9bf918d0437e36eb0fa7d4
Jason Pimble <jpimble@japi.net>
2021-11-22 20:32:05 -0500

    Starting support for multiple text entries for a control.

--- emu-control-labels/Emulators/Emu_config.py
+++ emu-control-labels/Emulators/Emu_config.py
@@ -47,7 +47,7 @@ class Emu_game_config:
         self.num_players = None
         self.alternating = None
         self.font = None
-        
+
         if game_id is not None:
             self.load_game_config(main_config.data_directory + "/configs/emulators/" + emulator_id + "/" + game_id + ".xml", game_id)
     
@@ -90,6 +90,8 @@ class Emu_game_config:
                         color = text.attrib.get("color")
                         font = text.attrib.get("font")
                         font_size = text.attrib.get("font_size")
+                        text_id = text.attrib.get("id")
+                        default_id = text.attrib.get("default")
 
                         if font_size is None:
                             font_size = self.font_size
@@ -99,13 +101,19 @@ class Emu_game_config:
 
                         if color is None:
                             color = default_color
-                            
-                        c_label.set_text(text = label_text, width=width, height=height, lang=lang, color=color, font=font, font_size=font_size)
+
+                        if default_id == "1":
+                            default_id = True
+                        else:
+                            default_id = False
+
+                        c_label.set_text(text = label_text, width=width, height=height, lang=lang, color=color, 
+                                         font=font, font_size=font_size, text_id=text_id, default=default_id)
                 
                         self.controls_label[control_id] = c_label
         except Exception as e:
-#            print("Error loading config: " + filename)
-#            print(e)
+            print("Error loading config: " + filename)
+            print(e)
             pass
     
     def lookup_label(self, control_id):
@@ -175,26 +183,51 @@ class Control_label:
         if text is not None:
             self.set_text(text=text, unverified=unverified)
         
-    def get_text_label(self, width=None, height=None, lang="en_us"):
-        key = self.build_key(width, height, lang)
-        if key in self.text_labels:
+    def get_text_label(self, width=None, height=None, lang="en_us", text_id=None):
+        key = self.get_text_key(width=width, height=height, lang=lang, text_id=text_id)
+        if key is not None:
             return self.text_labels[key]
-        
+
+        return None
+
+    def get_text_key(self, width=None, height=None, lang="en_us", text_id=None):
+        key = self.build_key(width, height, lang, text_id)
+        if key in self.text_labels:
+            return key
+
         key = self.build_key(lang=lang)
         if key in self.text_labels:
-            return self.text_labels[key]
+            return key
         
+        return None
+
+    def get_default_key(self, width=None, height=None, lang="en_us"):
+        key = self.build_key(width, height, lang)
+        if key in self.text_labels:
+            return key
+
+        key = self.build_key(lang=lang)
+        if key in self.text_labels:
+            return key
+
         return None
     
     def get_image(self, width=None, height=None, lang="en_us"):
         return None
     
-    def set_text(self, text=None, width=None, height=None, lang="en_us", color=None, font=None, font_size=None, unverified=None):
-        key = self.build_key(width, height, lang)
+    def set_text(self, text=None, width=None, height=None, lang="en_us", color=None, font=None, font_size=None, unverified=None, text_id=None, default=False):
+        key = self.build_key(width, height, lang, text_id)
         text_label = Text_label(text=text, width=width, height=height, lang=lang, color=color, font=font, font_size=font_size, unverified=unverified)
         self.text_labels[key] = text_label
-    
-    def build_key(self, width=None, height=None, lang="en_us"):
+        if text_id is not None:
+            if default:
+                key = self.build_key(width, height, lang)
+                self.text_labels[key] = text_label
+            elif self.get_default_key(width, height, lang) is None:
+                key = self.build_key(width, height, lang)
+                self.text_labels[key] = text_label
+            
+    def build_key(self, width=None, height=None, lang="en_us", text_id=None):
         key = ""
         if width is None:
             key = "None"
@@ -210,7 +243,12 @@ class Control_label:
             key = key + "|None"
         else:
             key = key + "|" + str(lang)
-    
+
+        if text_id is None:
+            key = key + "|None"
+        else:
+            key = key + "|" + str(text_id)
+
         return key
     
 class Text_label:
